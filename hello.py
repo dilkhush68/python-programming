f135f4f0665992feca3b318a6d8061369f4859b8
Dilkhush Kumar <149616680+dilkhush68@users.noreply.github.com>
2024-05-07 22:45:52 +0530

    case sensitivity

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 print("hello world")
 
 
-# printing python variables
-a=" dilkhush kumar"  #string variables
-b= 654    # integer variables
-c= 77.25  # floating point variables
+# #  1.printing python variables
+# a=" dilkhush kumar"  #string variables
+# b= 654    # integer variables
+# c= 77.25  # floating point variables
 
-print(a)    # print sring
-print(b)    # print integer
-print(c)    # print floating
+# print(a)    # print sring
+# print(b)    # print integer
+# print(c)    # print floating
+
+
+# 2.case sensitivity
+age=20
+Age=20
+print("age =",age)
+print("Age =",age)
