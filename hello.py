4758ebfd346e72df10fd4dff78bf895f678a638c
Dilkhush Kumar <149616680+dilkhush68@users.noreply.github.com>
2024-05-07 22:39:39 +0530

    printing

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+
+# printing python variables
+a=" dilkhush kumar"  #string variables
+b= 654    # integer variables
+c= 77.25  # floating point variables
+
+print(a)    # print sring
+print(b)    # print integer
+print(c)    # print floating
