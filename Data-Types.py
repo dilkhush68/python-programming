5eac911a165752ca21a61771bbfaf403494ed2ee
Dilkhush Kumar <149616680+dilkhush68@users.noreply.github.com>
2024-05-07 23:44:43 +0530

    hell

---
+++ Data-Types.py
@@ -0,0 +1,13 @@
+# python numerid data  types
+out1 = 1  # int data type
+out2 = True # bool data type 
+out3 = 10.235 # float data type
+out4 = 10+3j  # complex data type
+print("the type of variable habing value ",out1,"is",type(out1))
+print("the type of variable habing value ",out2,"is",type(out2))
+print("the type of variable habing value ",out3,"is",type(out3))
+print("the type of variable habing value ",out4,"is",type(out4))
+
+
+
+
