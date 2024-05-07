c8ff64c34810d47fb01cebdc62bcea44ed27e0c8
Dilkhush Kumar <149616680+dilkhush68@users.noreply.github.com>
2024-05-07 23:04:28 +0530

    valid python variables

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 print("hello world")
 
 
-# #  1.printing python variables
+ #  1.printing python variables
 # a=" dilkhush kumar"  #string variables
 # b= 654    # integer variables
 # c= 77.25  # floating point variables
@@ -12,7 +12,36 @@ print("hello world")
 
 
 # 2.case sensitivity
-age=20
-Age=20
-print("age =",age)
-print("Age =",age)
+# age=20
+# Age=20
+# print("age =",age)
+# print("Age =",age)
+
+
+# a =str(120)
+# b= float(203)
+# c= int(526)
+# print("a=",a)
+# print("b=",b)
+# print("c=",c)
+
+#  3.multiple assignment
+# a=b=c=520
+# print(a,b,c)
+
+# x,y,z =10,60.3,"dilkhush"
+# print(x,y,z)
+
+# following are valid python variables names
+# counter =100
+# _count =200
+# name1 ="dilkhush"
+# name2 ="kumar"
+# Age = 20
+# zara_aslary = 10000
+# print(counter)
+# print(_count)
+# print(name1)
+# print(name2)
+# print(Age)
+# print(zara_aslary)
